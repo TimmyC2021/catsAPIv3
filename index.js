2eb008971712ebf2cb7b57e9988ece1fe1a50dc5
TimmyC <77976627+TimmyC2021@users.noreply.github.com>
2021-04-03 00:02:36 +0100

    03 Working on post

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
-import cats from './cats';
+import cats from './cats.js';
+
 
 const app = express();
 
@@ -8,20 +9,35 @@ app.use(cors());
 
 app.use(express.json());
 
+
 app.get('/cats', (reqCats, resCats) => {
-  resCats.send('resCats')
+  resCats.send(cats);
+  // resCats.send('The End')
 });
 
 app.get('/cats/:catID', (reqGetCat, resGetCat) => {
-  resGetCat.send('resGetCat')
-});
+  const catID = reqGetCat.params.catID;
+  const result = cats.filter( cat => {
+    if (cat.id == catID) {
+      return cat
+    }
+  })
+  if (result.length > 0) {
+    resGetCat.send(result)
+  } else {
+  resGetCat.send(`No such cat`)
+}});
 
-app.post('/cats/:catID', (reqPostCat, resPostCat) => {
+app.post('/cats/', (reqPostCat, resPostCat) => {
+  const toLog = reqPostCat.body[0].id;
+  console.log(toLog);
+  
+
   resPostCat.send('resPostCat')
 });
 
-app.delete('/cats/:catID', (reqDeleteCat, resdeleteCat) => {
-  resdeleteCat.send('resdeleteCat')
+app.delete('/cats/:catID', (reqDeleteCat, resDeleteCat) => {
+  resDeleteCat.send('resDeleteCat')
 });
 
 app.listen(8080);
